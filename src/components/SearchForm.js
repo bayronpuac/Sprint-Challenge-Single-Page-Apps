2ca6bee48d2deab65764a5a8c07757215a5275c7
Bayron Puac <bayronpuac@gmail.com>
2019-09-20 13:38:31 -0500

    added a few adjustments.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,24 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-export default function SearchForm({ onSearch }) {
-  const [query, setQuery] = useState({
-    name: ""
-  })
-  const handleInputChange = (event) => {
-    setQuery({ ...query, name: event.target.value })
-  }
+const Search = ({ values, errors, touched, status }) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    if (status) {
+      setUser([...user, status]);
+    }
+  }, [status]);
 
   return (
-    <section className="search-form">
-      <form onSubmit={() => onSearch(query)}>
-        <input
-          onChange={handleInputChange}
-          placeholder="name"
-          value={query.name}
-          name="name"
-        />
-        <button type="submit">Search</button>
-      </form>
-    </section>
+    <div className="user-form">
+      <Form>
+        <Field type="text" name="name" placeholder="Place name here" />
+        {touched.name && errors.name && (
+          <p className="error">{errors.name}</p>
+        )}    
+
+        <button type="submit">Search </button>
+      </Form>
+      {user.map(user => (
+        <ul key={user.id}>
+          <li>Name: {user.name}</li>
+        </ul>
+      ))}
+    </div>
   );
-}
+};
+const FormikUserForm = withFormik({
+  mapPropsToValues({ name }) {
+    return {
+      name: name || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log(res);
+        setStatus(res.data.result);
+        resetForm();
+      })
+      .catch(err => console.log(err.res));
+  }
+})(Search);
+console.log("This is the HOC", FormikUserForm);
+export default FormikUserForm;
